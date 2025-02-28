cc4d55c12cb7f5d9895d3c9784d73193d43f17c4
FedorProgrammer123 <fedorlohkin@gmail.com>
2025-02-28 14:45:41 +0300

    DatabaseAndMainWindow

---
+++ MasterPolApp/Pages/PartnerListViewPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterPolApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnerListViewPage.xaml
+    /// </summary>
+    public partial class PartnerListViewPage : Page
+    {
+        public PartnerListViewPage()
+        {
+            InitializeComponent();
+            PartnerListView.ItemsSource = Data.DatabaseMasterPolEntities.GetContext().PartnerImport.ToList();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.AddEditPage());
+        }
+
+        private void DataGridButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.HistoryDataGridPage());
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.AddEditPage());
+        }
+    }
+}
